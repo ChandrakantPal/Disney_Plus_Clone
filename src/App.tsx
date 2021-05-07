f5398e6f6d1e6b6843eeb3f26604d5d102d9c219
ChandrakantPal <sanjay161097@gmail.com>
2021-05-07 23:31:23 +0530

    router setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
+import { Switch, Route } from 'react-router-dom'
+
+import Login from './components/Login'
 import './App.css'
 
 function App() {
-  return <div className="App"></div>
+  return (
+    <div className="App">
+      <Switch>
+        <Route exact path="/" component={Login} />
+      </Switch>
+    </div>
+  )
 }
 
 export default App
