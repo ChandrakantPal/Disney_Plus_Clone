e91a5c76822b99a58992ec8883b551ce08ba7bea
ChandrakantPal <sanjay161097@gmail.com>
2021-05-08 00:12:20 +0530

    cta logo one added

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -4,6 +4,9 @@ const Login = () => {
   return (
     <Container>
       <Content>
+        <CTA>
+          <CTALogoOne src="/images/cta-logo-one.svg" alt="" />
+        </CTA>
         <BgImage />
       </Content>
     </Container>
@@ -42,6 +45,16 @@ const BgImage = styled.div`
   right: 0;
   left: 0;
   z-index: -1;
+`
+
+const CTA = styled.div``
+
+const CTALogoOne = styled.img`
+  margin-bottom: 12px;
+  max-width: 600px;
+  min-height: 1px;
+  display: block;
+  width: 100%;
 `
 
 export default Login
