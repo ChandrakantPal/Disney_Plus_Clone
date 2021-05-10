31f48eb8200f3abcd0d4b61d9be9d35c838609d1
ChandrakantPal <sanjay161097@gmail.com>
2021-05-10 22:24:02 +0530

    header component imported and used

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import { Switch, Route } from 'react-router-dom'
 
 import Login from './components/Login'
 import './App.css'
+import Header from './components/Header'
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Switch>
         <Route exact path="/" component={Login} />
       </Switch>
